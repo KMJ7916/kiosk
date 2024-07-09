9a10325851d9dc6393295d1d5ead4c3e42e6d7c0
kmj <kimmj7916@gmail.com>
2024-07-09 11:37:02 +0900

    fetch: menu

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cafe/',include("cafe.urls"))
+    path('cafe/',include("cafe.urls")),
+    path('', views.index, name='index'),
 ]
